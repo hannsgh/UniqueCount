5fd276e953ca042766934b30e60712b09ad3aad1
hannsgh <hannsgh@bk.ru>
2021-04-21 14:36:47 +0400

    add read file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class App {
@@ -14,19 +18,29 @@ public class App {
             fis = new FileInputStream("src/main/resources/properties.properties");
             property.load(fis);
 
-           // String paths = property.getProperty("paths");
+           String paths = property.getProperty("paths");
 
             long before = System.currentTimeMillis();
             System.out.println(property.getProperty("paths"));
-           // long unique = count(paths);
+           long unique = count(paths);
             long after = System.currentTimeMillis();
 
-          //  System.out.println("Существует " + unique + " уникальных строк ");
+            System.out.println("Существует " + unique + " уникальных строк ");
             System.out.println(" Время работы приложения : " + (after - before) / 1_000 + " sec");
 
 
         } catch (IOException e) {
             System.err.println("ОШИБКА: Файл свойств отсуствует!");
         }
+
+
+        }
+
+    static long count(String paths) throws IOException {
+        long unique = Files.lines(Paths.get(paths), Charset.defaultCharset())
+                .flatMap(line -> Arrays.stream(line.split("\n")))
+                .distinct()
+                .count();
+        return unique;
     }
-}
+    }
